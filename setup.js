bd0cba4410e1963316887500086683fbab19bc7c
Digital Forecast <admin@pres-lie.com>
2018-06-21 10:53:58 -0400

    worked a bit on setup.js

--- setup.js
+++ setup.js
@@ -1,7 +1,25 @@
 var fs = require("fs");
 function setUpFiles() {
-    if(fs.existsSync(__dirname+"modules")) {
-        fs.mkdirSync("modules");
+    const defaultConfig = {
+        "repository": {
+            "description": "This is the default repository for module downloads, config updates, etc.",
+            "value": "https://github.com/echo-discord/"
+        },
+        "defaultModuleDownloads": {
+            "description": "These are the default modules to be downloaded for the Discord version. Run on first setup.",
+            "value": [
+                "blu-discord",
+                "blu-database",
+                "blu-permissions",
+                "blu-mapper",
+                "blu-essentials"
+            ]
+        }
+    }
+    if(!fs.existsSync(__dirname+"/modules")) {
+        fs.mkdirSync(__dirname+"/modules");
+    }
+    if(!fs.existsSync(__dirname+"/config.json")) {
+        fs.writeFileSync(__dirname+"/config.json", defaultConfig);
     }
 }
-console.log(__dirname);
